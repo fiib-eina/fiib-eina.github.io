e3ffc342d1f11655e7e17a409ef652987e81d95c
Miguel Ángel Latre Abadía <latre@unizar.es>
2025-01-16 17:43:47 +0100

    Add comments

--- src/03_practicas/p0/saluda.py
+++ src/03_practicas/p0/saluda.py
@@ -1,5 +1,8 @@
 """El programa pide un nombre al usuario.
 Luego escribe en la pantalla "Hola, NOMBRE"."""
 
+# Pide al usuario que escriba su nombre, lo lee de teclado y lo guarda en «nombre»
 nombre = input("¿Cómo te llamas? ")
+
+# Saluda al usuario escribiendo un mensaje personalizado en la pantalla 
 print("Hola,", nombre)
