3cb100cac4d2bc0f722593d8817e67b4b473bb10
SureSRM <sergiozz95@gmail.com>
2025-01-16 19:44:59 +0100

    Built site for gh-pages

--- src/03_practicas/p0/ej3.py
+++ src/03_practicas/p0/ej3.py
@@ -1,4 +1,4 @@
-x = int(input("Introduce un número: "))
+x = float(input("Introduce un número: "))
 
 if x >= 0:
     print(x)
---
+++ src/03_practicas/p0/saluda.py
@@ -0,0 +1,10 @@
+""" Sergio Marín Segura - 622612
+El programa pide un nombre al usuario.
+Luego escribe en la pantalla "Hola, NOMBRE".
+"""
+
+# Pide al usuario que escriba su nombre, lo lee de teclado y lo guarda en «nombre»
+nombre = input("¿Cómo te llamas? ")
+
+# Saluda al usuario escribiendo un mensaje personalizado en la pantalla 
+print("Hola,", nombre)
